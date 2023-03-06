b6e2f97deb0e373432655c4aed591f0c0f572c11
Thomas <taadamson33@gmail.com>
2023-03-06 02:14:16 -0500

    added routes + bugfixes

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
         isDecimal: true,
@@ -36,7 +36,7 @@ Product.init(
       },
 
     },
-    catagory_id: {
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
